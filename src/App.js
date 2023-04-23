7e6ef2d8b9679248b51e92e638f0b5237fcc311d
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2023-04-23 14:30:16 +0530

    App(topbar)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { ColorModeContext,useMode } from "./theme";
+import { CssBaseline,ThemeProvider } from "@mui/material";
+import Topbar from "./components/Topbar";
 function App() {
-  return (
+  const [theme,colorMode]=useMode();
+  return (<ColorModeContext.Provider value={colorMode}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline /> 
     <div className="app">
+      <main className="content">
+        <Topbar/>
+        </main>
       </div>
+      </ThemeProvider> 
+      </ColorModeContext.Provider>
   );
 }
 
